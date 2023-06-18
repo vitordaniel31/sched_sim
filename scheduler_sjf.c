aa61ad22bd522287e26c249daf8ed74f45dbb477
Adriel <adriel.fsantos@outlook.com>
2023-06-18 13:01:33 -0300

    Corrige comentário

--- scheduler_sjf.c
+++ scheduler_sjf.c
@@ -2,21 +2,21 @@
 
 #include "queue.h" // contem funções uteis para filas
 #include "proc.h"  // possui as funções dos processos
-#include "stats.h" // possui as funções de estatisticas 
-#include "utils.h" // possui funções uteis 
+#include "stats.h" // possui as funções de estatisticas
+#include "utils.h" // possui funções uteis
 
 // Utilizando as variáveis globais definidas no 'main'
-extern struct queue * ready;    // fila de aptos
-extern struct queue * ready2;   // segunda fila de aptos
-extern struct queue * blocked;  // fila de bloqueados
-extern struct queue * finished; // fila de finalizados
+extern struct queue *ready;    // fila de aptos
+extern struct queue *ready2;   // segunda fila de aptos
+extern struct queue *blocked;  // fila de bloqueados
+extern struct queue *finished; // fila de finalizados
 // NOTE: essa fila de finalizados é utilizada apenas para
 // as estatisticas finais
 
 // variavel global que indica o tempo maximo que um processo pode executar ao todo
 extern int MAX_TIME;
 
-struct proc * scheduler(struct proc * current)
+struct proc *scheduler(struct proc *current)
 {
     struct proc *selected;
     struct proc *currentIteratedProcess;
@@ -27,34 +27,34 @@ struct proc * scheduler(struct proc * current)
         // Verificando o estado em que o processo executando está
         switch (current->state)
         {
-            // Caso esteja 'apto' é porque saiu por preempção
-            case READY:
-                // Adiciona ao fim da fila de aptos
-                enqueue(ready, current);
-                // Realizando as estatisticas para o processo que entra na fila de
-                // aptos
-                count_ready_in(current);
-                break;
-            // Caso o processo tenha saído como 'bloqueado', é porque fez uma E/S
-            case BLOCKED:
-                // Adicionando no final da fila de bloqueados
-                enqueue(blocked, current);
+        // Caso esteja 'apto' é porque saiu por preempção
+        case READY:
+            // Adiciona ao fim da fila de aptos
+            enqueue(ready, current);
+            // Realizando as estatisticas para o processo que entra na fila de
+            // aptos
+            count_ready_in(current);
+            break;
+        // Caso o processo tenha saído como 'bloqueado', é porque fez uma E/S
+        case BLOCKED:
+            // Adicionando no final da fila de bloqueados
+            enqueue(blocked, current);
 
-                // Realizando as estatisticas para o processo que entra na fila de
-                // bloqueados
-                count_blocked_in(current);
-                break;
-            // Caso o processo tenha finalizado, estado 'finished'
-            case FINISHED:
-                // Adicionando-o na fila de finalizados
-                enqueue(finished, current);
+            // Realizando as estatisticas para o processo que entra na fila de
+            // bloqueados
+            count_blocked_in(current);
+            break;
+        // Caso o processo tenha finalizado, estado 'finished'
+        case FINISHED:
+            // Adicionando-o na fila de finalizados
+            enqueue(finished, current);
 
-                // Realizando as estatisticas para o processo que entra na fila de
-                // finalizados
-                count_finished_in(current);
-                break;
-            default:
-                printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+            // Realizando as estatisticas para o processo que entra na fila de
+            // finalizados
+            count_finished_in(current);
+            break;
+        default:
+            printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
         }
     }
 
@@ -77,7 +77,7 @@ struct proc * scheduler(struct proc * current)
     selected = ready->head;
     currentIteratedProcess = selected->next;
 
-    // Percorre a lista de processos aptos para selecionar o mais longo
+    // Percorre a lista de processos aptos para selecionar o mais curto
     while (currentIteratedProcess)
     {
         if (currentIteratedProcess->remaining_time < selected->remaining_time)
